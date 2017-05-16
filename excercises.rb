44cc31445be7a8d63d320ac256984e7d7bcd8102
Don Cooper <don_cooper@hotmail.com>
2017-05-16 15:54:32 -0400

    On excercise 4

--- excercises.rb
+++ excercises.rb
@@ -1,4 +1,5 @@
 # Excercise 0
+#------------
 
 fav_colours = ["Blue", "Red", "Yellow", "Green"]
 
@@ -17,9 +18,9 @@ word_definitions = {
 }
 
 movie_years = {
-  "Bloodsport" => 1988,
-  "UHF" => 1989,
-  "Revenge of the Nerds" => 1984
+  "Bloodsport" => "1988",
+  "UHF" => "1989",
+  "Revenge of the Nerds" => "1984"
 }
 
 city_populations = {
@@ -34,4 +35,71 @@ cousins_ages = {
   "Justin" => 22
 }
 
+
 # Excercise 1
+#------------
+
+# puts coin_toss
+#
+# puts fav_colours[0]
+#
+# puts friend_ages.sort
+#
+# friend_ages << 0
+#
+# puts movie_years["Bloodsport"]
+
+
+# Excercise 2
+#------------
+
+# puts colour_symbols.last
+#
+# city_populations["Manila"] = "24 million"
+#
+# puts coin_toss.reverse!
+#
+# puts city_populations["Tokyo"]
+#
+# artists.each do |artist|
+#   puts "I think #{artist} is great."
+# end
+
+
+# Excercise 3
+#------------
+
+# puts artists[0..1]
+
+# movie_years.each do |name, year|
+#   puts "#{name} came out in #{year}."
+# end
+
+# puts friend_ages.sort.reverse!
+
+# movie_years["Beauty and the Beast"] = "1991, 2017"
+# puts movie_years
+
+
+# Excercise 4
+#------------
+
+# cousins_ages.each do |name, age|
+#   if age < 40
+#     puts age
+#   end
+# end
+
+# friend_ages.sort.reverse
+# puts friend_ages.last
+
+# heads_only = []
+# coin_toss.each do |toss|
+#   if toss == "heads"
+#     heads_only << toss
+#   end
+# end
+# puts heads_only.length
+
+artists["Dylan"].delete
+puts artists
