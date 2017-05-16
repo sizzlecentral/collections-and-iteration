41ee6838c899cb0b124f8e6b438c077ef98fa823
Don Cooper <don_cooper@hotmail.com>
2017-05-16 13:36:40 -0400

    First commit

---
+++ excercises.rb
@@ -0,0 +1,37 @@
+# Excercise 0
+
+fav_colours = ["Blue", "Red", "Yellow", "Green"]
+
+friend_ages = [31, 36, 33, 42]
+
+coin_toss = ["heads", "heads", "tails", "tails", "heads"]
+
+artists = ["Clapton", "Hendrix", "Dylan"]
+
+colour_symbols = [:blue, :red, :yellow, :green]
+
+word_definitions = {
+  "Hash" => "a mess, jumble, or muddle",
+  "Rocket" => "a space capsule or vehicle put into orbit by such devices",
+  "Code" => "a system used for brevity or secrecy of communication"
+}
+
+movie_years = {
+  "Bloodsport" => 1988,
+  "UHF" => 1989,
+  "Revenge of the Nerds" => 1984
+}
+
+city_populations = {
+  "Tokyo" => "38 million",
+  "Jakarta" => "30 million",
+  "Delhi" => "25 million"
+}
+
+cousins_ages = {
+  "Melanie" => 40,
+  "Ryan" => 38,
+  "Justin" => 22
+}
+
+# Excercise 1
